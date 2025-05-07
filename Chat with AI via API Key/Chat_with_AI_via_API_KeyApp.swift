06389edd4e93e25979e80cb6f919c8a3cc24f61d
Gan Tu <dev@tugan.io>
2025-05-07 10:20:03 -0400

    Initial Commit

---
+++ Chat with AI via API Key/Chat_with_AI_via_API_KeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_with_AI_via_API_KeyApp.swift
+//  Chat with AI via API Key
+//
+//  Created by Gan Tu on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Chat_with_AI_via_API_KeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat with AI via API Key/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat with AI via API Key
+//
+//  Created by Gan Tu on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
